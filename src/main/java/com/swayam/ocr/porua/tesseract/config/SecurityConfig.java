a610255d7b57e0f40a609264d84576f5ca6ab782
Palash Ray <paawak@gmail.com>
2020-11-02 21:29:47 +0530

    using AuthenticationFilter instead of custom filter

--- src/main/java/com/swayam/ocr/porua/tesseract/config/SecurityConfig.java
+++ src/main/java/com/swayam/ocr/porua/tesseract/config/SecurityConfig.java
@@ -1,13 +1,25 @@
 package com.swayam.ocr.porua.tesseract.config;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -18,11 +30,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private GoogleAuthenticationFilter googleAuthenticationFilter;
+    private AuthenticationManager authenticationManager;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-	http.cors().and().addFilterAfter(googleAuthenticationFilter, BasicAuthenticationFilter.class)
+	http.cors().and().addFilterAfter(authenticationFilter(), BasicAuthenticationFilter.class)
 		.authorizeRequests(a -> a.antMatchers("/", "/error", "/webjars/**").permitAll().anyRequest().authenticated())
 		.exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
     }
@@ -37,9 +49,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	return source;
     }
 
-    // @Bean
-    // public JwtDecoder jwtDecoder() {
-    // return JwtDecoders.fromOidcIssuerLocation("https://accounts.google.com");
-    // }
+    @Bean
+    public AuthenticationConverter authenticationConverter() {
+	return (HttpServletRequest request) -> new UsernamePasswordAuthenticationToken("Hardcoded Full Name", "", new ArrayList<>());
+    }
+
+    @Bean
+    public AuthenticationFilter authenticationFilter() {
+	AuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler() {
+
+	    @Override
+	    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+		// do nothing
+	    }
+
+	};
+	AuthenticationFilter filter = new AuthenticationFilter(authenticationManager, authenticationConverter());
+	filter.setSuccessHandler(successHandler);
+	return filter;
+    }
 
 }
