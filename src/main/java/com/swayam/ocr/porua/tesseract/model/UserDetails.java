a478568ae1620ea5ed6a424711be2e6e4adff8da
Palash Ray <paawak@gmail.com>
2020-11-29 09:26:30 +0530

    persist role as string

--- src/main/java/com/swayam/ocr/porua/tesseract/model/UserDetails.java
+++ src/main/java/com/swayam/ocr/porua/tesseract/model/UserDetails.java
@@ -2,6 +2,8 @@ package com.swayam.ocr.porua.tesseract.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,6 +27,7 @@ public class UserDetails {
     private String name;
 
     @Column
+    @Enumerated(EnumType.STRING)
     private UserRole role;
 
 }
