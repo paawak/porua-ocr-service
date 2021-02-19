eb82b6af5425563fae73584898f10fb9641ddfd6
Palash Ray <paawak@gmail.com>
2021-02-20 01:45:04 +0530

    minor changes

--- src/main/resources/sql/cut-large-sql.sh
+++ src/main/resources/sql/cut-large-sql.sh
@@ -9,6 +9,7 @@ lines=`echo "$wc" | awk -F[" "] '{print $1}'`
 
 echo "********* Total lines: $lines, split by $2"
 
+fileCount=1
 start=1
 while [ $start -lt $lines ]
 do  
@@ -19,10 +20,11 @@ do
 	end=$lines
     fi
 
-  sedCmd="sed -n '$start,$end""p' $1 > $1-"$start"-"$end
+  sedCmd="sed -n '$start,$end""p' $1 > $1__"$fileCount"__"$start"-"$end
   eval $sedCmd
 #  sed -n '3,6p' /path/to/file
   start=$(($end+1))
+  fileCount=$(($fileCount+1))
 #  sleep 1
 done
 
